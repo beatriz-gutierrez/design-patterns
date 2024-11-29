00568b29c76bd80d7b528b50a603a0bee4cb010c
BeaGuti <beatriz.gutierrez@gmail.com>
2024-11-29 16:37:15 +0100

    update state example

--- state/example1.py
+++ state/example1.py
@@ -14,45 +14,61 @@ class Node:
         else:
             return self.tag_name
 
-
+# Available states: OpenTag, CloseTag, TextNode and ChildNode
 class OpenTag:
+
     def process(self, remaining_string, parser):
+
         i_start_tag = remaining_string.find("<")
         i_end_tag = remaining_string.find(">")
+       
         tag_name = remaining_string[i_start_tag + 1 : i_end_tag]
         node = Node(tag_name, parser.current_node)
+        
         parser.current_node.children.append(node)
         parser.current_node = node
         print("Change to ChildNode state")
         parser.state = ChildNode()
+
         return remaining_string[i_end_tag + 1 :]
 
 
 class CloseTag:
+
     def process(self, remaining_string, parser):
+       
         i_start_tag = remaining_string.find("<")
         i_end_tag = remaining_string.find(">")
+       
         assert remaining_string[i_start_tag + 1] == "/"
         tag_name = remaining_string[i_start_tag + 2 : i_end_tag]
         assert tag_name == parser.current_node.tag_name
+        
         parser.current_node = parser.current_node.parent
         print("Change to ChildNode state")
         parser.state = ChildNode()
+       
         return remaining_string[i_end_tag + 1 :].strip()
 
 
 class TextNode:
+
     def process(self, remaining_string, parser):
+       
         i_start_tag = remaining_string.find("<")
         text = remaining_string[:i_start_tag]
+       
         parser.current_node.text = text
         print("Change to ChildNode state")
         parser.state = ChildNode()
+        
         return remaining_string[i_start_tag:]
 
 
 class ChildNode:
+
     def process(self, remaining_string, parser):
+       
         stripped = remaining_string.strip()
         if stripped.startswith("</"):
             print("Change to CloseTag state")
@@ -63,22 +79,29 @@ class ChildNode:
         else:
             print("Change to TextNode state")
             parser.state = TextNode()
+        
         return stripped
 
 
 class FirstTag:
+
     def process(self, remaining_string, parser):
+
         i_start_tag = remaining_string.find("<")
         i_end_tag = remaining_string.find(">")
+
         tag_name = remaining_string[i_start_tag + 1 : i_end_tag]
         root = Node(tag_name)
+
         parser.root = parser.current_node = root
         print("Change to ChildNode state")
         parser.state = ChildNode()
+
         return remaining_string[i_end_tag + 1 :]
 
 
 class Parser:
+
     def __init__(self, parse_string):
         self.parse_string = parse_string
         self.root = None
@@ -95,10 +118,11 @@ class Parser:
     def start(self):
         self.process(self.parse_string)
 
+
 if __name__ == "__main__":
     
-
     with open(os.path.join("state", "simplified.xml")) as file:
+        
         contents = file.read()
         p = Parser(contents)
         p.start()
